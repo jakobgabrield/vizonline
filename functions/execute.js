c307a8800cda098cf5d2a38d60a08877b237dbb4
Jakob Deutsch <jakobgabrield@aol.com>
2022-05-04 17:10:22 -0400

    Fix Bug

--- functions/execute.js
+++ functions/execute.js
@@ -4,11 +4,11 @@ const { languageToCompiler } = require('../languageMaps');
 const executeProgram = async (filepath, args) => {
     let run = "";
     if (args == "") {
-        // run = `cd ./functions/viz && eval $(opam config env) && ./vizOutput online_programs/${filepath} 2> errors.txt || true`;
-        run = `cd ./functions/viz && eval $(opam config env) && ./vizOutput online_programs/${filepath}`;
+        run = `cd ./functions/viz && eval $(opam config env) && ./vizOutput online_programs/${filepath} 2> errors.txt || true`;
+        // run = `cd ./functions/viz && eval $(opam config env) && ./vizOutput online_programs/${filepath}`;
     } else if (args == "build") {
-        // run = `cd ./functions/viz && eval $(opam config env) && ./viz online_programs/${filepath} 2> errors.txt || true`
-        run = `cd ./functions/viz && eval $(opam config env) && ./viz online_programs/${filepath}`;
+        run = `cd ./functions/viz && eval $(opam config env) && ./viz online_programs/${filepath} 2> errors.txt || true`
+        // run = `cd ./functions/viz && eval $(opam config env) && ./viz online_programs/${filepath}`;
     } else {
         run = `cd ./functions/viz && eval $(opam config env) && dune exec -- vc online_programs/${filepath} ${args}`;
     }
