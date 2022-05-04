112bcae5bf72a6cf2662e55ab642734a1c377bb5
Jakob Deutsch <jakobgabrield@aol.com>
2022-05-04 16:49:31 -0400

    More error message

--- functions/execute.js
+++ functions/execute.js
@@ -7,7 +7,8 @@ const executeProgram = async (filepath, args) => {
         // run = `cd ./functions/viz && eval $(opam config env) && ./vizOutput online_programs/${filepath} 2> errors.txt || true`;
         run = `cd ./functions/viz && eval $(opam config env) && ./vizOutput online_programs/${filepath}`;
     } else if (args == "build") {
-        run = `cd ./functions/viz && eval $(opam config env) && ./viz online_programs/${filepath} 2> errors.txt || true`
+        // run = `cd ./functions/viz && eval $(opam config env) && ./viz online_programs/${filepath} 2> errors.txt || true`
+        run = `cd ./functions/viz && eval $(opam config env) && ./viz online_programs/${filepath}`;
     } else {
         run = `cd ./functions/viz && eval $(opam config env) && dune exec -- vc online_programs/${filepath} ${args}`;
     }
