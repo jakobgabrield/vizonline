ba8ce510682b540169d5e0b761b1d6956211215c
Jakob Deutsch <jakobgabrield@aol.com>
2022-05-10 11:06:55 -0400

    App.js Update

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ const App = () => {
     <div className="page">
       <div style={{ display: 'flex', gap: '10px' }}>
         <a href="https://github.com/4115-viz/viz" target="_blank" rel="noopener noreferrer">Viz Compiler Source Code</a><br/><br/>
-        <a href="https://github.com/4115-viz/viz/blob/master/writeups/LRM/Viz_LRM.pdf" target="_blank" rel="noopener noreferrer">Viz Compiler Reference Manual</a>
+        <a href="https://github.com/4115-viz/viz/blob/master/writeups/LRM/Viz_LRM.pdf" target="_blank" rel="noopener noreferrer">Viz Language Reference Manual</a>
       </div>
       <h3 className="title">Viz Online</h3>
       <div style={{ display: 'flex', height: '30px', justifyContent: 'center', alignItems: 'center', gap: '10px'}}>
